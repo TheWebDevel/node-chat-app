25bd1676e50d4f829feb8eb3daff80a03d91429d
Sathish Kumar <sathishkumar0416@gmail.com>
2018-01-06 05:09:35 +0530

    Mustache

--- public/js/index.js
+++ public/js/index.js
@@ -11,20 +11,25 @@ socket.on('disconnect', function() {
 });
 
 socket.on('newMessage', function(message) {
+    var template = jQuery('#message-template').html();
     var formattedTime = moment(message.createdAt).format('hh:mm a');
-    var li = jQuery('<li></li>')
-    li.text(`${message.from} ${formattedTime}: ${message.text}`)
-    jQuery('#messages').append(li);
+    var html = Mustache.render(template, {
+        text : message.text,
+        time : formattedTime,
+        from : message.from
+    });
+    jQuery('#messages').append(html);
 });
 
 socket.on('newLocationMessage', function(message) {
-    var li = jQuery('<li></li>')
+    var template = jQuery('#location-message-template').html();
     var formattedTime = moment(message.createdAt).format('hh:mm a');
-    var a = jQuery('<a target="_blank">Current Location<a>')
-    li.text(`${message.from} ${formattedTime}: `)
-    a.attr('href', message.url);
-    li.append(a);
-    jQuery('#messages').append(li);
+    var html = Mustache.render(template, {
+        text : message.url,
+        time : formattedTime,
+        from : message.from
+    });
+    jQuery('#messages').append(html);
 });
 
 jQuery('#message-form').on('submit', function (e) {
