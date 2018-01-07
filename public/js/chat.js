8e762c56ba71675bf8fe573e55e5c9567cbaa2e5
Sathish Kumar <sathishkumar0416@gmail.com>
2018-01-07 07:58:26 +0530

    Complete

--- public/js/chat.js
+++ public/js/chat.js
@@ -18,12 +18,28 @@ function scrollToBottom () {
 }
 
 socket.on('connect', function() {
-    console.log('connected to server');
+    var params = jQuery.deparam(window.location.search);
 
+      socket.emit('join', params, function (err) {
+        if (err) {
+          alert(err);
+          window.location.href = '/';
+        } else {
+          console.log('No error');
+        }
+      });
 });
 
 socket.on('disconnect', function() {
     console.log('Disconnected from server');
+});
+
+socket.on('updateUserList', function (users) {
+    var ol = jQuery('<ol></ol>');
+    users.forEach(function (user) {
+        ol.append(jQuery('<li></li>').text(user));
+    });
+    jQuery('#users').html(ol);
 });
 
 socket.on('newMessage', function(message) {
@@ -54,8 +70,7 @@ jQuery('#message-form').on('submit', function (e) {
     e.preventDefault();
     var messageTextBox = jQuery('[name=message]');
     socket.emit('createMessage', {
-        from : 'user',
-        text : jQuery('[name=message]').val()
+        text : messageTextBox.val()
     }, function () {
         messageTextBox.val('');
     });
